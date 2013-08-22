a1be325ed9a443db8767497890fd974c26aa49b6
Bojan <bn@novp.com>
2013-08-22 16:00:11 +0200

    Added recent and archived posts to controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-	 @posts = Post.order('created_at DESC').limit(5)
+    @posts = Post.order('created_at DESC')
+    @recent_posts = @posts.limit(5)
+    @archived_posts = Post.find(:all, :order => 'created_at DESC').group_by { |post| post.created_at.strftime("%B %Y")}
   end
 end
