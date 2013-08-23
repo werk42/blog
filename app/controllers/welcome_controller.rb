e1e66b8a05e4f81c1010e7f450a1f2ed20fd4728
Bojan <bn@novp.com>
2013-08-23 16:02:21 +0200

    Added links to posts to their individual page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,10 +2,6 @@ class WelcomeController < ApplicationController
   def index
     @posts = Post.order('created_at DESC')
     @recent_posts = @posts.limit(5)
-    @archived_posts = Post.find(:all, :order => 'created_at DESC').group_by { |post| post.created_at.strftime("%B %Y")}
-  end
-
-  def show
-    @post = Post.find(params[:id])
+    @archived_posts = Post.find(:all, :id, :order => 'created_at DESC').group_by { |post| post.created_at.strftime("%B %Y")}
   end
 end
