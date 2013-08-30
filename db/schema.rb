e3f5dd4f82b2ee5a4be4575dffce6ff3af948777
Bojan <bn@novp.com>
2013-08-30 13:36:28 +0200

    Fixed migration issue

--- db/schema.rb
+++ db/schema.rb
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20130830104024) do
   end
 
   create_table "tags", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
