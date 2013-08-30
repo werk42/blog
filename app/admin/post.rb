0b2f97ae1d4ba940bd7b38c8d06d4a8e8e40a36b
Bojan <bn@novp.com>
2013-08-30 13:07:42 +0200

    Added tags and associtations between posts

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -3,6 +3,7 @@ ActiveAdmin.register Post do
     f.inputs do
       f.input :title
       f.input :body, as: :html_editor
+      f.input :tags
     end
 
     f.buttons
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ActiveRecord::Base
-	attr_accessible :title, :body, :tag
+	attr_accessible :title, :body, :tags
+  
+  has_many :taggings
+  has_many :tags, :through => :taggings
 end
