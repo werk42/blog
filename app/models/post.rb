845364d0c17cd7f7923f40d5cbea94b436c7acfb
Bojan <bn@novp.com>
2013-08-30 14:38:00 +0200

    Tags are now shown on post

--- app/models/post.rb
+++ app/models/post.rb
@@ -17,7 +17,7 @@ class Post < ActiveRecord::Base
   def assign_tags
     if @tag_names
       self.tags = @tag_names.split(/\s+/).map do |name|
-        Tag.find_or_create_by_name(name)
+        Tag.find_or_create_by_name(name.strip)
       end
     end
   end
